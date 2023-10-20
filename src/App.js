3df52345d95903e3fc619b3657af609173aa49d3
Navaneeth Sai Patti <navaneethsaipatti@gmail.com>
2023-10-20 15:53:56 +0530

    Added Routing,Category Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {Routes, Route} from 'react-router-dom';
+import Navigation from './Components/Routes/Navigation/Navigation';
+import Home from './Components/Routes/Home-Route/Home';
 
-function App() {
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='/shop' element={<h1>Shop</h1>} />
+        </Route>
+    </Routes>
+    </>
+  )
+
 }
 
 export default App;
