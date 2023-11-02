f79f26e9fcd2bb03338851bce375aafce31432ed
Navaneeth Sai Patti <navaneethsaipatti@gmail.com>
2023-11-02 11:41:58 +0530

    Tried to increase the page loading speed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-
+import { lazy,Suspense } from 'react';
 import {Routes, Route} from 'react-router-dom';
-import Navigation from './Routes/Navigation/Navigation';
-import Home from './Routes/Home-Route/Home';
-import AuthComponent from './Routes/Authentication/AuthComponent';
-import Shop from './Routes/Shop/ShopComponent';
-import Checkout from './Routes/Checkout/CheckOut';
+// import Navigation from './Routes/Navigation/Navigation';
+// import Home from './Routes/Home-Route/Home';
+// import AuthComponent from './Routes/Authentication/AuthComponent';
+// import Shop from './Routes/Shop/ShopComponent';
+// import Checkout from './Routes/Checkout/CheckOut';
 
+const Home = lazy(() => import('./Routes/Home-Route/Home'));
+const AuthComponent = lazy(() => import('./Routes/Authentication/AuthComponent'));
+const Shop = lazy(() => import('./Routes/Shop/ShopComponent'));
+const Checkout = lazy(() => import('./Routes/Checkout/CheckOut'));
+const Navigation = lazy(() => import('./Routes/Navigation/Navigation'));
 
 const App = () => {
   
   return (
-    <>
+    <Suspense>
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
@@ -19,7 +24,7 @@ const App = () => {
         <Route path='checkout' element={<Checkout/>} />
       </Route>
     </Routes>
-    </>
+    </Suspense>
   )
 
 }
